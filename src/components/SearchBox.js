36e4ae5b755256f979866e07f2a53e8a516c55be
Behnam Shahriari <shahriaribehnam@gmail.com>
2022-03-26 15:18:34 +0200

    PWA

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,9 +4,9 @@ import React from 'react';
 const SearchBox = ({searchfield, searchChange}) => {
 	return(
 		<div className='pa2 '>
-			<input className="pa3 ba b--green bg-lightest-blue" onChange={searchChange} type="search" placeholder="search robots"/>
+			<input arial-label="Search Robots" className="pa3 ba b--green bg-lightest-blue" onChange={searchChange} type="search" placeholder="search robots" />
 		</div>
 		)
 }
 
-export default SearchBox;
+export default SearchBox; 
