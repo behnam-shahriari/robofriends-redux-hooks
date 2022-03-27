7a851fffaa87954946737e838c637c7fdf74a265
Behnam Shahriari <shahriaribehnam@gmail.com>
2022-03-27 09:58:48 +0300

    Changing app title

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ class Header extends Component {
 	render() {
 		return(
 			<div>
-				<h1 className='f1'>RoboFriends</h1>
+				<h1 className='f1'>RoboFriends - Behnam Shahriari</h1>
 				<CounterButton color={'red'} />
 			</div>
 			)
